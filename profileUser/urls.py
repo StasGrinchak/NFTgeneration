1865caf39ea278c8a91f7d286751d66d0f2b71d3
StasGrinchak <grinchakstanislav@gmail.com>
2021-09-15 09:16:12 +0300

    create login, logout and register

--- profileUser/urls.py
+++ profileUser/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('profileUser.urls'))
+    path('home/', views.home, name='home'),
+    path('register/', views.register_request, name='register'),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
 ]
